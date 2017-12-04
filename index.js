4b07cbec131d1449addf28cbe0e9068068d46bc1
凌恒 <jiakun.dujk@alibaba-inc.com>
2017-12-04 10:01:42 +0800

    feat: prevent duplicate hook

--- index.js
+++ index.js
@@ -10,45 +10,49 @@ const assert = require('assert');
 const compile = {};
 
 hook.on('beforeRequire', (info) => {
-	const name = info.name;
-	const version = info.version;
-	const handle = handlers[name];
-	if (handle && handle.length) {
-		for(let item of handle) {
-			if (semver.satisfies(version, item.version)) {
-				item.handler(function loadModule(filename) {
-					return require(path.join(info.path, filename));
-				}, function replaceSource(filename, replace) {
-					const realPath = path.join(info.path, filename);
-					if (typeof replace === 'function') {
-						compile[realPath] = replace;
-					}	else if (typeof replace === 'string' && fs.existsSync(replace)) {
-						compile[realPath] = function() {
-							return fs.readFileSync(replace, 'utf8');
-						}
-					}
-				});
-			}
-		}
-	}
+  const name = info.name;
+  const version = info.version;
+  const handle = handlers[name];
+  if (handle && handle.length) {
+    for (let item of handle) {
+      if (semver.satisfies(version, item.version)) {
+        if (item.handler.wrapped) continue;
+
+        item.handler(function loadModule(filename) {
+          return require(path.join(info.path, filename));
+        }, function replaceSource(filename, replace) {
+          const realPath = path.join(info.path, filename);
+          if (typeof replace === 'function') {
+            compile[realPath] = replace;
+          } else if (typeof replace === 'string' && fs.existsSync(replace)) {
+            compile[realPath] = function() {
+              return fs.readFileSync(replace, 'utf8');
+            }
+          }
+        });
+
+        item.handler.wrapped = true;
+      }
+    }
+  }
 });
 
 hook.on('beforeCompile', (info, callback) => {
-	const filename = info.filename;
-	if (compile[filename]) {
-		callback(compile[filename](info.content));
-	} else {
-		callback(info.content);
-	}
+  const filename = info.filename;
+  if (compile[filename]) {
+    callback(compile[filename](info.content));
+  } else {
+    callback(info.content);
+  }
 });
 
 module.exports = function(name, version, handler) {
-	assert(typeof handler === 'function');
-	if (!handlers[name]) {
-		handlers[name] = [];
-	}
-	handlers[name].push({
-		version,
-		handler
-	});
+  assert(typeof handler === 'function');
+  if (!handlers[name]) {
+    handlers[name] = [];
+  }
+  handlers[name].push({
+    version,
+    handler
+  });
 }
